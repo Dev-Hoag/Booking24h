848da919d39bf01b3e74475dcf53949a2d313ec4
thanhthuy15-git <nguyenthanhthanh15205@gmail.com>
2025-10-11 20:56:02 +0700

    Add about and facilities pages with initial content and structure

---
+++ about.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TJ Hotel - ABOUT</title>
+    
+    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css">
+    
+    <?php require('inc/links.php'); ?>
+</head>
+<body class="bg-light">
+
+    <?php require('inc/header.php'); ?>
+
+    <?php require('inc/footer.php'); ?>
+
+</body>
+</html>
